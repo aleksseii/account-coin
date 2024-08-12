11feff2903c57a32a1cf9f449d32e70dec70ae83
a.shkel <a.shkel@tinkoff.ru>
2024-08-12 03:44:35 +0300

    Added writing error in response utils

--- api/api.go
+++ api/api.go
@@ -1,5 +1,11 @@
 package api
 
+import (
+	"encoding/json"
+	logger "github.com/sirupsen/logrus"
+	"net/http"
+)
+
 type CoinBalanceParams struct {
 	username string
 }
@@ -13,3 +19,26 @@ type Error struct {
 	code    uint16
 	message string
 }
+
+func writeError(responseWriter http.ResponseWriter, code uint16, message string) {
+	response := Error{
+		code:    code,
+		message: message,
+	}
+	responseWriter.Header().Set("Content-Type", "application/json; charset=utf-8")
+	responseWriter.WriteHeader(int(code))
+
+	err := json.NewEncoder(responseWriter).Encode(response)
+	if err != nil {
+		logger.Fatalf("Failed to write response, err: %v", err)
+	}
+}
+
+var (
+	RequestErrorHandler = func(responseWriter http.ResponseWriter, err error) {
+		writeError(responseWriter, http.StatusBadRequest, err.Error())
+	}
+	InternalErrorHandler = func(responseWriter http.ResponseWriter, err error) {
+		writeError(responseWriter, http.StatusInternalServerError, "Unexpected internal error occurred")
+	}
+)
